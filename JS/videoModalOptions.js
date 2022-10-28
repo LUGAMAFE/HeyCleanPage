a5b06299922ad64625164f8d5a362b7668a86353
Alfonso Tort <alfonso.tort@anahuac.mx>
2022-10-28 01:16:40 -0500

    mejoras a la pagina

--- JS/videoModalOptions.js
+++ JS/videoModalOptions.js
@@ -1,3 +1,15 @@
-window.addEventListener('DOMContentLoaded',function(){	
-            new ModalVideo(".videoModal", {channel: 'youtube', youtube:{ cc_load_policy: 1, modestbranding: 1, rel: 0,controls:1, autoplay: 1, allowFullScreen: false}});
+document.addEventListener("DOMContentLoaded", () => {
+  window.addEventListener("DOMContentLoaded", function () {
+    new ModalVideo(".videoModal", {
+      channel: "youtube",
+      youtube: {
+        cc_load_policy: 1,
+        modestbranding: 1,
+        rel: 0,
+        controls: 1,
+        autoplay: 1,
+        allowFullScreen: false,
+      },
+    });
+  });
 });
