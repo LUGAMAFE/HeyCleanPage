678a64e2d14e267c85f257a65009d895fbecd5c3
Alfonso Tort <alfonso.tort@anahuac.mx>
2022-11-21 01:56:15 -0600

    cambios a la animación y lighthouse

--- JS/animacion.js
+++ JS/animacion.js
@@ -6,8 +6,9 @@ gsap.to(logito, {
   keyframes: [
     { rotate: 360, duration: 1 },
     { duration: 1, x: 0, rotate: 0, delay: 1 },
-    { duration: 0.5, scale: 7, delay: 1, autoAlpha: 0.3 },
+    { duration: 0.5, scale: 6, delay: 1, autoAlpha: 0.3 },
     { duration: 0.2, y: -1000 },
+    { scale: 0 },
   ],
 });
 
